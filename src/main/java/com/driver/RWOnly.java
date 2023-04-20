7c87833de918d0799e3996988a1cd0a9fd94f0ed
Boby <kanyal182017@gmail.com>
2023-04-20 12:39:37 +0530

    Assignment done

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public String getName() {
+        return name;
+    }
+
+    // Method 2 - Setter
+    public void setName(String N)
+    {
+        // This keyword refers to current instance itself
+        this.name = N;
+    }
 }
